ac14775c82fb816ca891a1fad30076a1d4ad9141
abrasov <abrasovs@yandex.ru>
2021-08-23 18:49:23 +0300

    feat: add new version with dark theme

--- drum-kit/js/main.js
+++ drum-kit/js/main.js
@@ -1,4 +1,11 @@
-const keys = document.querySelectorAll('.key')
+const keys = document.querySelectorAll('.drums-key')
+const drums = document.querySelector('.drums-content')
+const page = document.querySelector('.page')
+const themeBtn = document.querySelector('.theme-button')
+
+themeBtn.onclick = () => {
+  page.classList.toggle('dark-theme')
+}
 
 const playing = (src) => {
   const audio = new Audio()
@@ -7,30 +14,29 @@ const playing = (src) => {
   audio.play()
 }
 
-const startSound = (event) => {
-  const letter = event.target.dataset.letter
-  const src = `assets/sounds/${letter}.wav`
+const startSoundD = (event) => {
+  const name = event.target.dataset.name
+  const src = `assets/sounds/${name}.wav`
   playing(src)
-
-  event.target.classList.add('key-active')
+  event.target.classList.add('drums-key--active')
 }
-const stopSound = (event) => {
-  event.target.classList.remove('key-active')
+const stopSoundD = (event) => {
+  event.target.classList.remove('drums-key--active')
 }
-const startMouse = (event) => {
-  if (event.target.classList.contains('key')) {
-    startSound(event)
+const startMouseD = (event) => {
+  if (event.target.classList.contains('drums-key')) {
+    startSoundD(event)
   }
   keys.forEach((key) => {
-    key.addEventListener('mouseover', startSound)
-    key.addEventListener('mouseout', stopSound)
+    key.addEventListener('mouseover', startSoundD)
+    key.addEventListener('mouseout', stopSoundD)
   })
 }
-const stopMouse = () => {
+const stopMouseD = () => {
   keys.forEach((key) => {
-    key.classList.remove('key-active')
-    key.removeEventListener('mouseover', startSound)
-    key.removeEventListener('mouseout', stopSound)
+    key.classList.remove('drums-key--active')
+    key.removeEventListener('mouseover', startSoundD)
+    key.removeEventListener('mouseout', stopSoundD)
   })
 }
 const handleKeyDown = (event) => {
@@ -38,9 +44,9 @@ const handleKeyDown = (event) => {
   const pressedKey = event.code.slice(-1)
   keys.forEach((key) => {
     if (key.dataset.letter === pressedKey) {
-      key.classList.add('key-active')
-      const letter = key.dataset.letter
-      const src = `assets/sounds/${letter}.wav`
+      key.classList.add('drums-key--active')
+      const name = key.dataset.name
+      const src = `assets/sounds/${name}.wav`
       playing(src)
     }
   })
@@ -48,12 +54,11 @@ const handleKeyDown = (event) => {
 
 const handleKeyUp = (event) => {
   keys.forEach((key) => {
-    key.classList.remove('key-active')
+    key.classList.remove('drums-key--active')
   })
 }
 
-document.addEventListener('mousedown', startMouse)
-document.addEventListener('mouseup', stopMouse)
-
+document.addEventListener('mousedown', startMouseD)
+document.addEventListener('mouseup', stopMouseD)
 window.addEventListener('keydown', handleKeyDown)
 window.addEventListener('keyup', handleKeyUp)
