f6a4c146a9d1fbcb23b99d5a30c495e261432a0b
abrasov <abrasovs@yandex.ru>
2021-08-23 21:13:34 +0300

    feat: add second screen with fun mode

--- drum-kit/js/main.js
+++ drum-kit/js/main.js
@@ -2,10 +2,29 @@ const keys = document.querySelectorAll('.drums-key')
 const drums = document.querySelector('.drums-content')
 const page = document.querySelector('.page')
 const themeBtn = document.querySelector('.theme-button')
+const drumsBtn = document.querySelector('.drums-btn')
+const funBtn = document.querySelector('.fun-btn')
+const fun = document.querySelector('.fun-content')
+const funKeys = document.querySelectorAll('.fun-key')
 
 themeBtn.onclick = () => {
   page.classList.toggle('dark-theme')
 }
+const setDrums = () => {
+  fun.classList.add('hidden')
+  drums.classList.remove('hidden')
+  drumsBtn.classList.add('setting-btn--active')
+  funBtn.classList.remove('setting-btn--active')
+}
+const setFun = () => {
+  drums.classList.add('hidden')
+  fun.classList.remove('hidden')
+  funBtn.classList.add('setting-btn--active')
+  drumsBtn.classList.remove('setting-btn--active')
+}
+
+drumsBtn.addEventListener('click', setDrums)
+funBtn.addEventListener('click', setFun)
 
 const playing = (src) => {
   const audio = new Audio()
@@ -51,7 +70,6 @@ const handleKeyDown = (event) => {
     }
   })
 }
-
 const handleKeyUp = (event) => {
   keys.forEach((key) => {
     key.classList.remove('drums-key--active')
@@ -62,3 +80,37 @@ document.addEventListener('mousedown', startMouseD)
 document.addEventListener('mouseup', stopMouseD)
 window.addEventListener('keydown', handleKeyDown)
 window.addEventListener('keyup', handleKeyUp)
+
+const startSoundFun = (event) => {
+  const letter = event.target.dataset.letter
+  const src = `assets/sounds/${letter}.mp3`
+  playing(src)
+  event.target.classList.add('fun-key--active')
+}
+const stopSoundFun = (event) => {
+  event.target.classList.remove('fun-key--active')
+}
+const startMouseFun = (event) => {
+  if (event.target.classList.contains('fun-key')) {
+    startSoundFun(event)
+  }
+  funKeys.forEach((key) => {
+    key.addEventListener('mouseover', startSoundFun)
+    key.addEventListener('mouseout', stopSoundFun)
+  })
+}
+const stopMouseFun = () => {
+  funKeys.forEach((key) => {
+    key.classList.remove('fun-key--active')
+    key.removeEventListener('mouseover', startSoundFun)
+    key.removeEventListener('mouseout', stopSoundFun)
+  })
+}
+document.addEventListener('mousedown', startMouseFun)
+document.addEventListener('mouseup', stopMouseFun)
+
+
+
+
+
+
